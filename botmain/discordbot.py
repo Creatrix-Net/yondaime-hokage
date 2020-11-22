0f7804535f25ec8309afa7fe950474464b6b40a9
Dhruva Shaw <dhruvashaw@gmail.com>
2020-11-22 21:01:30 +0530

    added spank command

--- botmain/discordbot.py
+++ botmain/discordbot.py
@@ -1,11 +1,15 @@
-import discord
-from discord.ext import commands
 import datetime
 import random
-
-from urllib import parse, request
 import re
+from os import listdir
+from os.path import isfile, join
+from pathlib import Path
+from urllib import parse, request
 
+import discord
+from discord.ext import commands
+
+BASE_DIR = Path(__file__).resolve().parent.parent
 TOKEN = 'Nzc5NTU5ODIxMTYyMzE1Nzg3.X7iTqA.PEmxShgoueoJgaE6BvQatCCT4XM'
 bot = commands.Bot(command_prefix='.', description="This is a Helper Bot only for the freelance server")
 
@@ -19,12 +23,13 @@ async def sum(ctx, numOne: int, numTwo: int):
 
 @bot.command()
 async def info(ctx):
-    embed = discord.Embed(title=f"{ctx.guild.name}", description="{ctx.guild.description}", timestamp=datetime.datetime.utcnow(), color=discord.Color.blue())
+    embed = discord.Embed(title=f"{ctx.guild.name}", description=f"{ctx.guild.description if ctx.guild.description else '' }", timestamp=datetime.datetime.utcnow(), color=discord.Color.blue())
     embed.add_field(name="Server created at", value=f"{ctx.guild.created_at}")
-    embed.add_field(name="Server Owner", value=f"{ctx.guild.owner}")
-    embed.add_field(name="Server Region", value=f"{ctx.guild.region}")
+    embed.add_field(name="Server Owner", value=f"<@{ctx.guild.owner_id}>")
+    embed.add_field(name="Server Region", value=f"{str(ctx.guild.region).upper() if ctx.guild.region else 'No region set till now'}")
     embed.add_field(name="Server ID", value=f"{ctx.guild.id}")
-    embed.set_thumbnail(url=f"{ctx.guild.icon}")
+    embed.add_field(name="Members(Bots included)", value=ctx.guild.member_count)
+    embed.set_thumbnail(url=str(ctx.guild.icon_url))
     await ctx.send(embed=embed)
 
 @bot.command(aliases=['8ball'])
@@ -52,18 +57,27 @@ async def _8ball(ctx, *, question):
         "Very doubtful",
         "Stoping asking me fucking studip questions !!!! I need peace bitch!"
     ]
-    await ctx.send(f'**Your question**: {question}\n **Here you go my Answer** : {random.choice(responses)}')
+    for i in random.choice(responses).split(' '):
+        await ctx.send(f'**{i.upper()}**')
 
-
+#spank
 @bot.command()
-async def youtube(ctx, *, search):
-    query_string = parse.urlencode({'search_query': search})
-    html_content = request.urlopen('http://www.youtube.com/results?' + query_string)
-    # print(html_content.read().decode())
-    search_results = re.findall('href=\"\\/watch\\?v=(.{11})', html_content.read().decode())
-    print(search_results)
-    # I will put just the first result, you can loop the response to show more results
-    await ctx.send('https://www.youtube.com/watch?v=' + search_results[0])
+async def spank(ctx, member = ''):
+    if member == '':
+        desc=f'<@{ctx.author.id}> spanks itself!!! LOL!'
+    elif member[:2] == '<@':
+        desc=f'<@{ctx.author.id}> spanks {member} !!! Damm!'
+    else:
+        desc=f'<@{ctx.author.id}> spanks itself!!! LOL!'
+    onlyfiles = [f for f in listdir(join(BASE_DIR,'botmain','discord_bot_images','spank'))]
+
+
+    embed = discord.Embed(description=desc, timestamp=datetime.datetime.utcnow())
+    image_name=random.choice(onlyfiles)
+
+    file = discord.File(join(BASE_DIR,'botmain','discord_bot_images','spank',image_name), filename=image_name)
+    embed.set_image(url=f"attachment://{image_name}")
+    await ctx.send(file=file,embed=embed)
 
 # Events
 @bot.event
@@ -84,7 +98,6 @@ async def on_message(message):
     if message.author == bot.user:
         return
     if "fuck" in message.content.lower():
-        # in this case don't respond with the word "Tutorial" or you will call the on_message event recursively
         await message.channel.send(f'Fuck off!!! <@{message.author.id}>')
         await bot.process_commands(message)
 

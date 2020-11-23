ee8d1de0e6a6d37bb1463e5dadb1022c90140f22
Dhruva Shaw <dhruvashaw@gmail.com>
2020-11-23 21:20:54 +0530

    Added the server class

--- botmain/models.py
+++ botmain/models.py
@@ -6,3 +6,6 @@ from django.utils.translation import gettext_lazy as _
 class servers(models.Model):
     servername = models.CharField(_('Server Name'),null=True,max_length=500)
     server_user_data = models.JSONField(_('Server Users Data'),null=False,default=dict)
+
+    def __str__(self):
+        return self.servername

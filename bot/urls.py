5454be8335f030af0c59b064bbb159e941e67f87
Dhruva Shaw <dhruvashaw@gmail.com>
2020-11-24 22:50:43 +0530

    minor

--- bot/urls.py
+++ bot/urls.py
@@ -7,7 +7,7 @@ from botmain.views import home
 
 
 urlpatterns = [ 
-    url(r'^discord/', include('discord_auth_data.urls')),
+    path('discord/', include('discord_auth_data.urls')),
     path('admin/', admin.site.urls),
     path('', home),
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)

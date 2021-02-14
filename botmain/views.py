26815b519251e3f5c31ed562222a2783ecb4eb5c
Dhruva Shaw <34207400+Dhruvacube@users.noreply.github.com>
2021-02-14 17:09:11 +0530

    Update views.py

--- botmain/views.py
+++ botmain/views.py
@@ -9,12 +9,4 @@ def home(request):
     return redirect(reverse('admin:index'))
 
 def home_view_alive(request):
-    auth_header = request.META.get('HTTP_AUTHORIZATION', '')
-    token_type, _, credentials = auth_header.partition(' ')
-    import base64
-    data = f'{settings.ID}:{settings.SECRECT}'
-    expected = data
-    if token_type != 'Basic' or credentials != expected:
-        return HttpResponse(status=401)
-    else:
-        return HttpResponse('<h1>Congratulations You succesfully kept the bot alive</h1>')
+    return HttpResponse('<h1>Congratulations You succesfully kept the bot alive</h1>')

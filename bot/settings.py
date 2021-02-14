c69372b35c808f5907df1bf119705adbce531999
Dhruva Shaw <34207400+Dhruvacube@users.noreply.github.com>
2021-02-14 23:10:08 +0530

    Update settings.py

--- bot/settings.py
+++ bot/settings.py
@@ -22,6 +22,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'django_admin_listfilter_dropdown',
+
 ]
 
 MIDDLEWARE = [

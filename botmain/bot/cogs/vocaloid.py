196ae36c422c43fe717d1f30a1f427840b2332e7
Dhruva Shaw <34207400+Dhruvacube@users.noreply.github.com>
2021-02-25 21:33:22 +0530

    Update vocaloid.py

--- botmain/bot/cogs/vocaloid.py
+++ botmain/bot/cogs/vocaloid.py
@@ -10,97 +10,135 @@ class Vocaloid(commands.Cog):
     @commands.command()
     async def rin(self, ctx):
         data = requests.get(url=self.endpoint + 'rin').json()['url']
-        await ctx.send(data)
+        e=discord.Embed()
+        e.set_image(url=data)
+        await ctx.send(embed=e)
 
     @commands.command()
     async def rin(self, ctx):
         data = requests.get(url=self.endpoint + 'rin').json()['url']
-        await ctx.send(data)
+        e=discord.Embed()
+        e.set_image(url=data)
+        await ctx.send(embed=e)
 
     @commands.command()
     async def una(self, ctx):
         data = requests.get(url=self.endpoint + 'una').json()['url']
-        await ctx.send(data)
+        e=discord.Embed()
+        e.set_image(url=data)
+        await ctx.send(embed=e)
 
     @commands.command()
     async def gumi(self, ctx):
         data = requests.get(url=self.endpoint + 'gumi').json()['url']
-        await ctx.send(data)
+        e=discord.Embed()
+        e.set_image(url=data)
+        await ctx.send(embed=e)
 
     @commands.command()
     async def ia(self, ctx):
         data = requests.get(url=self.endpoint + 'ia').json()['url']
-        await ctx.send(data)
+        e=discord.Embed()
+        e.set_image(url=data)
+        await ctx.send(embed=e)
 
     @commands.command()
     async def luka(self, ctx):
         data = requests.get(url=self.endpoint + 'luka').json()['url']
-        await ctx.send(data)
+        e=discord.Embed()
+        e.set_image(url=data)
+        await ctx.send(embed=e)
 
     @commands.command()
     async def fukase(self, ctx):
         data = requests.get(url=self.endpoint + 'fukase').json()['url']
-        await ctx.send(data)
+        e=discord.Embed()
+        e.set_image(url=data)
+        await ctx.send(embed=e)
 
     @commands.command()
     async def miku(self, ctx):
         data = requests.get(url=self.endpoint + 'miku').json()['url']
-        await ctx.send(data)
+        e=discord.Embed()
+        e.set_image(url=data)
+        await ctx.send(embed=e)
 
     @commands.command(name='len_vocaloid ',aliases=['len'])
     async def _len(self, ctx):
         data = requests.get(url=self.endpoint + 'len').json()['url']
-        await ctx.send(data)
+        e=discord.Embed()
+        e.set_image(url=data)
+        await ctx.send(embed=e)
 
     @commands.command()
     async def kaito(self, ctx):
         data = requests.get(url=self.endpoint + 'kaito').json()['url']
-        await ctx.send(data)
+        e=discord.Embed()
+        e.set_image(url=data)
+        await ctx.send(embed=e)
 
     @commands.command()
     async def teto(self, ctx):
         data = requests.get(url=self.endpoint + 'teto').json()['url']
-        await ctx.send(data)
+        e=discord.Embed()
+        e.set_image(url=data)
+        await ctx.send(embed=e)
 
     @commands.command()
     async def meiko(self, ctx):
         data = requests.get(url=self.endpoint + 'meiko').json()['url']
-        await ctx.send(data)
+        e=discord.Embed()
+        e.set_image(url=data)
+        await ctx.send(embed=e)
 
     @commands.command()
     async def yukari(self, ctx):
         data = requests.get(url=self.endpoint + 'yukari').json()['url']
-        await ctx.send(data)
+        e=discord.Embed()
+        e.set_image(url=data)
+        await ctx.send(embed=e)
 
     @commands.command()
     async def miki(self, ctx):
         data = requests.get(url=self.endpoint + 'miki').json()['url']
-        await ctx.send(data)
+        e=discord.Embed()
+        e.set_image(url=data)
+        await ctx.send(embed=e)
 
     @commands.command()
     async def lily(self, ctx):
         data = requests.get(url=self.endpoint + 'lily').json()['url']
-        await ctx.send(data)
+        e=discord.Embed()
+        e.set_image(url=data)
+        await ctx.send(embed=e)
 
     @commands.command()
     async def mayu(self, ctx):
         data = requests.get(url=self.endpoint + 'mayu').json()['url']
-        await ctx.send(data)
+        e=discord.Embed()
+        e.set_image(url=data)
+        await ctx.send(embed=e)
 
     @commands.command()
     async def aoki(self, ctx):
         data = requests.get(url=self.endpoint + 'aoki').json()['url']
-        await ctx.send(data)
+        e=discord.Embed()
+        e.set_image(url=data)
+        await ctx.send(embed=e)
 
     @commands.command()
     async def zola(self, ctx):
         data = requests.get(url=self.endpoint + 'zola').json()['url']
-        await ctx.send(data)
+        e=discord.Embed()
+        e.set_image(url=data)
+        await ctx.send(embed=e)
 
     @commands.command()
     async def diva(self, ctx):
         data = requests.get(url=self.endpoint + 'diva').json()['url']
-        await ctx.send(data)
+        e=discord.Embed()
+        e.set_image(url=data)
+        await ctx.send(embed=e)
 
 def setup(bot):
     bot.add_cog(Vocaloid(bot))

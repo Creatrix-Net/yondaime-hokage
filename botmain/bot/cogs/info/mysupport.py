baac5a094d5b04ffc3783f16b773edd35a2b345e
Dhruva Shaw <34207400+Dhruvacube@users.noreply.github.com>
2021-05-02 17:00:37 +0530

    Update mysupport.py

--- botmain/bot/cogs/info/mysupport.py
+++ botmain/bot/cogs/info/mysupport.py
@@ -9,12 +9,14 @@ class MySupport(commands.Cog, name="My Support"):
     
     @commands.command(description='Generates my invite link for your server')
     async def inviteme(self, ctx):
+        '''Generates my invite link for your server'''
         embed=discord.Embed(title='**Invite Link**',description=f'[My Invite Link!](https://discord.com/oauth2/authorize?client_id={self.bot.discord_id}&permissions=8&scope=bot%20applications.commands)')
         embed.set_thumbnail(url=ctx.bot.user.avatar_url)
         await ctx.send(embed=embed)
 
     @commands.command(description='Generates my support server invite')
     async def supportserver(self, ctx):
+        '''Generates my support server invite'''
         await ctx.send('**Here you go, my support server invite**')
         await ctx.send('https://discord.gg/g9zQbjE73K')
 

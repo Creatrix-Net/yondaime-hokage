09e3f1f733350bad66a74254ace4506861379d5a
Dhruva Shaw <dhruvashaw@gmail.com>
2021-06-19 13:56:28 +0530

    necessary formatting

--- minato_namikaze/botmain/bot/lib/classes/setup/ban.py
+++ minato_namikaze/botmain/bot/lib/classes/setup/ban.py
@@ -1,5 +1,6 @@
 import discord
 from discord.ext import menus
+
 from ..embed import Embed
 
 
@@ -7,29 +8,30 @@ class Ban(menus.Menu):
     def __init__(self, bot):
         super().__init__(timeout=60.0)
         self.bot = bot
-    
+
     async def send_initial_message(self, ctx, channel):
         embed = Embed(title=f"Want to log bans for the **{ctx.guild.name}** ?")
         embed.add_field(name="Yes", value=":white_check_mark:")
         embed.add_field(name="No", value=":negative_squared_cross_mark:")
         return await channel.send(embed=embed)
-    
+
     @menus.button('\N{WHITE HEAVY CHECK MARK}')
     async def on_add(self, payload):
-        bingo = discord.utils.get(self.ctx.guild.categories, name="Bingo Book") if discord.utils.get(self.ctx.guild.categories, name="Bingo Book") else False
+        bingo = discord.utils.get(self.ctx.guild.categories, name="Bingo Book") if discord.utils.get(
+            self.ctx.guild.categories, name="Bingo Book") else False
         if not bingo:
             bingo = await self.ctx.guild.create_category(
-                "Bingo Book", 
+                "Bingo Book",
                 reason="To log the the bans and unban events"
             )
         ban = await self.ctx.guild.create_text_channel(
-            "ban", 
+            "ban",
             topic='Bans of the server will be logged here.',
             category=discord.utils.get(
-                self.ctx.guild.categories, 
+                self.ctx.guild.categories,
                 name="Bingo Book")
         )
-        
+
         await self.channel.send(f'{ban.mention} channel **created** for logging the **ban** of the {self.ctx.guild.name} server.')
         e = Embed(
             description='This channel will be used to log the server bans.'
--- minato_namikaze/botmain/bot/lib/util/setup_server.py
+++ minato_namikaze/botmain/bot/lib/util/setup_server.py
@@ -1,64 +1,69 @@
 import discord
 
+
 def perms_dict(ctx):
-    admin_roles = [role for role in ctx.guild.roles if role.permissions.administrator and not role.managed]
+    admin_roles = [
+        role for role in ctx.guild.roles if role.permissions.administrator and not role.managed]
     overwrite_dict = {}
     for i in admin_roles:
         overwrite_dict[i] = discord.PermissionOverwrite(read_messages=True)
-    overwrite_dict.update({ctx.guild.default_role: discord.PermissionOverwrite(read_messages=False),ctx.guild.me: discord.PermissionOverwrite(read_messages=True)})
+    overwrite_dict.update({ctx.guild.default_role: discord.PermissionOverwrite(
+        read_messages=False), ctx.guild.me: discord.PermissionOverwrite(read_messages=True)})
     return admin_roles, overwrite_dict
+
 
 async def channel_creation(ctx):
     admin_roles, overwrite_dict = perms_dict(ctx)
-    
-    category = discord.utils.get(ctx.guild.categories, name="Admin / Feedback") if discord.utils.get(ctx.guild.categories, name="Admin / Feedback") else False
-    
-    #Feedback Channel
+
+    category = discord.utils.get(ctx.guild.categories, name="Admin / Feedback") if discord.utils.get(
+        ctx.guild.categories, name="Admin / Feedback") else False
+
+    # Feedback Channel
     if discord.utils.get(ctx.guild.channels, topic='Feedback given by the members of the servers will be logged here.'):
         feed_channel = discord.utils.get(
-            ctx.guild.channels,  
+            ctx.guild.channels,
             topic='Feedback given by the members of the servers will be logged here.'
         )
     else:
         feed_channel = False
 
-    #Ban
+    # Ban
     if discord.utils.get(ctx.guild.channels, topic='Bans of the server will be logged here.',):
         ban = discord.utils.get(
-            ctx.guild.channels, 
+            ctx.guild.channels,
             topic='Bans of the server will be logged here.'
-        ) 
+        )
     else:
         ban = False
-    
-    #Unban
+
+    # Unban
     if discord.utils.get(ctx.guild.channels, name="unban"):
         unban = discord.utils.get(
             ctx.guild.channels,
             topic='Unbans of the server will be logged here.',
-        )  
+        )
     else:
         unban = False
 
-    #Support
-    if discord.utils.get(category.channels, topic = 'This channel will be used as a support channel for thsi server.'):
+    # Support
+    if discord.utils.get(category.channels, topic='This channel will be used as a support channel for thsi server.'):
         support_channel = discord.utils.get(
-            ctx.guild.channels, 
-            topic = 'This channel will be used as a support channel for this server.'
-        )  
+            ctx.guild.channels,
+            topic='This channel will be used as a support channel for this server.'
+        )
     else:
         support_channel = False
-        
-    support_channel_roles = discord.utils.get(ctx.guild.roles, name="SupportRequired") if discord.utils.get(ctx.guild.roles, name="SupportRequired") else False
-    
+
+    support_channel_roles = discord.utils.get(ctx.guild.roles, name="SupportRequired") if discord.utils.get(
+        ctx.guild.roles, name="SupportRequired") else False
+
     if not category and support_channel and unban and ban and feed_channel:
-        category = await ctx.guild.create_category("Admin / Feedback", overwrites = overwrite_dict, reason="To log the admin and feedback events")
-    
-    #Bot Setup
+        category = await ctx.guild.create_category("Admin / Feedback", overwrites=overwrite_dict, reason="To log the admin and feedback events")
+
+    # Bot Setup
     if discord.utils.get(category.channels, name="bot-setup"):
-        botask = discord.utils.get(category.channels, name="bot-setup") 
+        botask = discord.utils.get(category.channels, name="bot-setup")
     else:
         botask = False
-    
-    return feed_channel,support_channel,support_channel_roles,ban,unban,botask
-    
+
+    return feed_channel, support_channel, support_channel_roles, ban, unban, botask

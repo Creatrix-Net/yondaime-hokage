ef022d4da3bb6a2940dda12199ce531fc18f0d41
Dhruva Shaw <34207400+Dhruvacube@users.noreply.github.com>
2021-07-05 17:53:46 +0530

    updated music commands

--- minato_namikaze/botmain/bot/cogs/music.py
+++ minato_namikaze/botmain/bot/cogs/music.py
@@ -1,13 +1,59 @@
 import discord
 import DiscordUtils
 from discord.ext import commands
-from ..lib import Embed
+from ..lib import Embed, ErrorEmbed
 
+class NoChannelProvided(commands.CommandError):
+    """Error raised when no suitable voice channel was supplied."""
+    pass
+
+
+class IncorrectChannelError(commands.CommandError):
+    """Error raised when commands are issued outside of the players session channel."""
+    pass
 
 class Music(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
         self.description = 'Listen to some soothing music!'
+    
+    async def cog_before_invoke(self, ctx: commands.Context):
+        """
+        Coroutine called before command invocation.
+        We mainly just want to check whether the user is in the players controller channel.
+        """
+        if ctx.command.name == 'join':
+            return
+        voice_state_author = ctx.author.voice
+        voice_state_me = ctx.me.voice
+        
+        if voice_state_author is not None:
+            if ctx.command.name == 'play':
+                if voice_state_me is None:
+                    await ctx.author.voice.channel.connect()
+            else:
+                if voice_state_author is None:
+                    raise NoChannelProvided
+        else:
+            raise NoChannelProvided
+        
+        return
+    
+    async def cog_command_error(self, ctx: commands.Context, error: Exception):
+        """Cog wide error handler."""
+        if isinstance(error, IncorrectChannelError):
+            return
+
+        if isinstance(error, NoChannelProvided):
+            return await ctx.send(embed=ErrorEmbed(description='You must be in a voice channel or provide one to connect to.'))
+
+    async def cog_check(self, ctx: commands.Context):
+        """Cog wide check, which disallows commands in DMs."""
+        if not ctx.guild:
+            await ctx.send(embed=ErrorEmbed(description='Music commands are not available in Private Messages.'))
+            return False
+
+        return True
     
     def songembed(self, song):
         e=Embed()
@@ -20,15 +66,26 @@ class Music(commands.Cog):
 
     @commands.command()
     async def join(self,ctx):
-        '''Joins the voice channel'''
-        await ctx.send('**Joined**')
+        '''Joins the voice channel'''        
+        voice_state_author = ctx.author.voice
+        voice_state_me = ctx.me.voice
+        if voice_state_author and voice_state_me and voice_state_author == voice_state_me:
+            return
+
+        if voice_state_author is None:
+            raise NoChannelProvided
+
         await ctx.author.voice.channel.connect() #Joins author's voice channel
+        await ctx.send('```**Joined**```')
 
     @commands.command()
     async def leave(self,ctx):
         '''Disconnects from a voice channel'''
-        await ctx.send('**Left**')
-        await ctx.voice_client.disconnect()
+        try:
+            await ctx.send('```**Left**```')
+            await ctx.voice_client.disconnect()
+        except:
+            pass
 
     @commands.command(usage='<music.url>')
     async def play(self,ctx, *, url):
@@ -59,21 +116,21 @@ class Music(commands.Cog):
         '''Pauses the current music playing'''
         player = self.bot.music.get_player(guild_id=ctx.guild.id)
         song = await player.pause()
-        await ctx.send(f"Paused {song.name}")
+        await ctx.send(f"```Paused {song.name}```")
 
     @commands.command()
     async def resume(self,ctx):
         '''Resumes music'''
         player = self.bot.music.get_player(guild_id=ctx.guild.id)
         song = await player.resume()
-        await ctx.send(f"Resumed {song.name}")
+        await ctx.send(f"```Resumed {song.name}```")
 
     @commands.command()
     async def stop(self,ctx):
         '''Stops the Music Player'''
         player = self.bot.music.get_player(guild_id=ctx.guild.id)
         await player.stop()
-        await ctx.send("Stopped")
+        await ctx.send("```Stopped```")
 
     @commands.command()
     async def loop(self,ctx):
@@ -81,9 +138,9 @@ class Music(commands.Cog):
         player = self.bot.music.get_player(guild_id=ctx.guild.id)
         song = await player.toggle_song_loop()
         if song.is_looping:
-            await ctx.send(f"Enabled loop for {song.name}")
+            await ctx.send(f"```Enabled loop for {song.name}```")
         else:
-            await ctx.send(f"Disabled loop for {song.name}")
+            await ctx.send(f"```Disabled loop for {song.name}```")
 
     @commands.command()
     async def queue(self,ctx):
@@ -118,9 +175,9 @@ class Music(commands.Cog):
         player = self.bot.music.get_player(guild_id=ctx.guild.id)
         data = await player.skip(force=True)
         if len(data) == 2:
-            await ctx.send(f"Skipped from {data[0].name} to {data[1].name}")
+            await ctx.send(f"```Skipped from {data[0].name} to {data[1].name}```")
         else:
-            await ctx.send(f"Skipped {data[0].name}")
+            await ctx.send(f"```Skipped {data[0].name}```")
 
     @commands.command(usage='<value between 1-100>')
     async def volume(self,ctx, vol):
@@ -134,7 +191,7 @@ class Music(commands.Cog):
         '''Song the specified song using its index value'''
         player = self.bot.music.get_player(guild_id=ctx.guild.id)
         song = await player.remove_from_queue(int(index))
-        await ctx.send(f"Removed {song.name} from queue")
+        await ctx.send(f"```Removed {song.name} from queue```")
 
 def setup(bot):
     bot.add_cog(Music(bot))

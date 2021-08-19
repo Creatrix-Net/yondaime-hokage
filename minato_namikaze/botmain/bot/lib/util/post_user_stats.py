045c1fc7e6c3efb6145960da2dd40451ee21617f
Dhruva Shaw <34207400+Dhruvacube@users.noreply.github.com>
2021-08-19 14:20:20 +0530

    discord.py2.0 update

--- minato_namikaze/botmain/bot/lib/util/post_user_stats.py
+++ minato_namikaze/botmain/bot/lib/util/post_user_stats.py
@@ -144,10 +144,6 @@ class PostStats:
                                      'Content-Type': 'application/json'},
                             json={'guildCount': guildsno}
                             )
-        h = await self.post(f'https://space-bot-list.xyz/api/bots/{self.bot.user.id}',
-                            headers={"Authorization": spacebot,
-                                     "Content-Type": "application/json"},
-                            json={"guilds": guildsno, "users": members})
 
         i = await self.post(f'https://api.voidbots.net/bot/stats/{self.bot.user.id}',
                             headers={"Authorization": voidbot,
@@ -179,8 +175,6 @@ class PostStats:
         e1.add_field(name='DiscordListSpace', value=f'{d} : [DiscordListSpace](https://discordlist.space/bot/{self.bot.user.id})')
         e1.add_field(name='DiscordBoats', value=f'{e} : [DiscordBoats](https://discord.boats/bot/{self.bot.user.id})')
         e1.add_field(name='DiscordBots', value=f'{f} : [DiscordBots](https://discord.bots.gg/bots/{self.bot.user.id}/)')
-
-        e1.add_field(name='Space Bots', value=f'{h} : [Space Bots](https://space-bot-list.xyz/bots/{self.bot.user.id})')
 
         e1.add_field(name='Void Bots', value=f'{i} : [Void Bots](https://voidbots.net/bot/{self.bot.user.id}/)')
         e1.add_field(name='Fates List', value=f'{j} : [Fates List](https://fateslist.xyz/minato/)')

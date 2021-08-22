7031e1747263577d0e8d78ffef7a3afdadbe78a8
Dhruva Shaw <34207400+Dhruvacube@users.noreply.github.com>
2021-08-22 20:35:17 +0530

    Update post_user_stats.py

--- minato_namikaze/botmain/bot/lib/util/post_user_stats.py
+++ minato_namikaze/botmain/bot/lib/util/post_user_stats.py
@@ -118,16 +118,17 @@ class PostStats:
             strict=True).parent.parent / join('text', 'images_list.txt')
         filepointer = open(imageslistdir)
         imageslist = filepointer.readlines()
+        shards=1
 
         a = await self.post(f'https://top.gg/api/bots/{self.bot.user.id}/stats',
                             headers={'Authorization': topken},
-                            data={'server_count': guildsno}
+                            data={'server_count': guildsno,"shard_count":shards
                             )
         b = await self.post(f'https://discordbotlist.com/api/v1/bots/{self.bot.user.id}/stats',
                             headers={'Authorization': dblst},
                             data={'guilds': guildsno, 'users': members}
                             )
-        c = await self.post(f'https://botsfordiscord.com/api/bot/{self.bot.user.id}',
+        c = await self.post(f'https://discords.com/bots/api/bot/{self.bot.user.id}',
                             headers={'Authorization': bfd,
                                      'Content-Type': 'application/json'},
                             json={'server_count': guildsno}
@@ -144,32 +145,28 @@ class PostStats:
         f = await self.post(f'https://discord.bots.gg/api/v1/bots/{self.bot.user.id}/stats',
                             headers={'Authorization': discordbotsgg,
                                      'Content-Type': 'application/json'},
-                            json={'guildCount': guildsno}
-                            )
-        h = await self.post(f'https://space-bot-list.xyz/api/bots/{self.bot.user.id}',
-                            headers={"Authorization": spacebot,
-                                     "Content-Type": "application/json"},
-                            json={"guilds": guildsno, "users": members})
+                            json={'guildCount': guildsno,"shardCount":shards}
+                           )
 
         i = await self.post(f'https://api.voidbots.net/bot/stats/{self.bot.user.id}',
                             headers={"Authorization": voidbot,
                                      "Content-Type": "application/json"},
-                            json={"server_count": guildsno})
+                            json={"server_count": guildsno,"shard_count":shards})
         j = await self.post(f'https://fateslist.xyz/api/v2/bots/{self.bot.user.id}/stats',
                             headers={"Authorization": fateslist,
                                      "Content-Type": "application/json"},
-                            json={"guild_count": guildsno, "user_count": members})
+                            json={"guild_count": guildsno, "user_count": members,"shard_count":shards})
         k = await self.post(f'https://bladebotlist.xyz/api/bots/{self.bot.user.id}/stats',
                             headers={"Authorization": bladebot,
                                      "Content-Type": "application/json"},
-                            json={"servercount": guildsno})
+                            json={"servercount": guildsno,"shardcount":shards})
         l = await self.post(f'https://bots.discordlabs.org/v2/bot/{self.bot.user.id}/stats',
                             headers={"Authorization": discordlabs,
                                      "Content-Type": "application/json"},
-                            json={"server_count": guildsno})
+                            json={"server_count": guildsno,"shard_count":shards})
         m = await self.post(f'https://api.infinitybotlist.com/bot/{self.bot.user.id}',
                             headers={"Authorization": infinity},
-                            json={"servers": guildsno, "botid":self.bot.user.id}
+                            json={"servers": guildsno, "botid":self.bot.user.id,'shards':shards}
                            )
 
         r = self.bot.get_channel(822472454030229545 if not self.bot.local else 870561578347540490)
@@ -185,9 +182,6 @@ class PostStats:
         e1.add_field(name='DiscordListSpace', value=f'{d} : [DiscordListSpace](https://discordlist.space/bot/{self.bot.user.id})')
         e1.add_field(name='DiscordBoats', value=f'{e} : [DiscordBoats](https://discord.boats/bot/{self.bot.user.id})')
         e1.add_field(name='DiscordBots', value=f'{f} : [DiscordBots](https://discord.bots.gg/bots/{self.bot.user.id}/)')
-
-        e1.add_field(name='Space Bots', value=f'{h} : [Space Bots](https://space-bot-list.xyz/bots/{self.bot.user.id})')
-
         e1.add_field(name='Void Bots', value=f'{i} : [Void Bots](https://voidbots.net/bot/{self.bot.user.id}/)')
         e1.add_field(name='Fates List', value=f'{j} : [Fates List](https://fateslist.xyz/minato/)')
         e1.add_field(name='BladeBotList', value=f'{k} : [BladeBotList](https://bladebotlist.xyz/bot/{self.bot.user.id}/)')

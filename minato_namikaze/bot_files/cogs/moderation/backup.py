2e39e02defa95dad4d277c464189d6225da6982d
Dhruva Shaw <34207400+Dhruvacube@users.noreply.github.com>
2021-12-22 00:09:53 +0530

    some updates

--- minato_namikaze/bot_files/cogs/moderation/backup.py
+++ minato_namikaze/bot_files/cogs/moderation/backup.py
@@ -1,14 +1,13 @@
 import discord
 from discord.ext import commands
 
-from ...lib import ChannelAndMessageId
+from ...lib import ChannelAndMessageId, BackupDatabse
 
 
 class BackUp(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
         self.description = "Create a backup for your server"
-        self.backup_channel = 0
 
     @property
     def display_emoji(self) -> discord.PartialEmoji:
@@ -18,9 +17,9 @@ class BackUp(commands.Cog):
     @commands.has_permissions(manage_guild=True)
     @commands.guild_only()
     @commands.cooldown(2, 60, commands.BucketType.guild)
-    async def backup(self):
-        """Create backup of the server"""
-        pass
+    async def backup(self, ctx):
+        backup_code = await BackupDatabse(ctx).create_backup()
+        await ctx.author.send(backup_code)
 
 
 def setup(bot):

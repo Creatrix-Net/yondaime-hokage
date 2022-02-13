c71dad9c5f4d1c609a88d53ab72a743d294ae7b2
Dhruva Shaw <dhruvashaw@gmail.com>
2022-02-13 23:09:33 +0530

    added vocaloids commands

--- minato_namikaze/slash/activities_vocaloid.py
+++ minato_namikaze/slash/activities_vocaloid.py
@@ -3,6 +3,7 @@ import discord
 
 from discord.abc import GuildChannel
 from discord import application_command_option, VoiceChannel
+from lib.functions import meek_api
 
 activities = [
     "youtube",
@@ -37,7 +38,19 @@ activities = [
 class Activities(discord.SlashCommand):
     """Get access to discord beta activities feature"""
 
-    activities: typing.Literal["youtube", "poker", "chess", "betrayal", "fishing", "letter-league","word-snack", "sketch-heads", "spellcast", "awkword","checkers", ] = discord.application_command_option(
+    activities: typing.Literal[
+        "youtube", 
+        "poker",
+        "chess", 
+        "betrayal",
+        "fishing",
+        "letter-league",
+        "word-snack", 
+        "sketch-heads", 
+        "spellcast", 
+        "awkword",
+        "checkers"
+        ] = discord.application_command_option(
             description="The type of activity",
             default="youtube",
     )
@@ -51,11 +64,44 @@ class Activities(discord.SlashCommand):
         link = await self.cog.bot.togetherControl.create_link(response.options.voice_channel.id, str(response.options.activities))
         await response.send_message(f"Click the blue link!\n{link}")
 
+class Vocaloids(discord.SlashCommand):
+    '''Get kawaii pictures of different vocaloids'''
+    vocaloid: typing.Optional[typing.Literal[
+        "rin",
+        "una",
+        "gumi",
+        "ia",
+        "luka",
+        "fukase",
+        "miku",
+        "len",
+        "kaito",
+        "teto",
+        "meiko",
+        "yukari",
+        "miki",
+        "lily",
+        "mayu",
+        "aoki",
+        "zola",
+        "diva"
+    ]] = discord.application_command_option(
+            description="Select you favourite vocaloid",
+            default="miku",
+    )
+
+    def __init__(self, cog):
+        self.cog = cog
+
+    async def callback(self, response: discord.SlashCommandResponse):
+        await response.send_message(embed=await meek_api(response.options.vocaloid))
+
 
 class ActivitiesCog(discord.Cog):
     def __init__(self, bot):
         self.bot = bot
         self.add_application_command(Activities(self))
+        self.add_application_command(Vocaloids(self))
 
 def setup(bot):
     bot.add_cog(ActivitiesCog(bot))

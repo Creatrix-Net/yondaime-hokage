fdd60c997f541a706f10832af5f42e6a99e1693d
Dhruva Shaw <dhruvashaw@gmail.com>
2022-02-13 12:06:33 +0530

    seperated application commands

--- minato_namikaze/discordbot.py
+++ minato_namikaze/discordbot.py
@@ -198,6 +198,16 @@ class MinatoNamikazeBot(commands.AutoShardedBot):
                 self._cache[guild.id][invite.code] = invite
         except:
             pass
+            
+        cog_dir = BASE_DIR / "cogs"
+        for filename in list(set(os.listdir(cog_dir))):
+            if os.path.isdir(cog_dir / filename):
+                for i in os.listdir(cog_dir / filename):
+                    if i.endswith(".py"):
+                        bot.load_extension(f'cogs.{filename.strip(" ")}.{i[:-3]}')
+            else:
+                if filename.endswith(".py"):
+                    bot.load_extension(f"cogs.{filename[:-3]}")
 
         if not self.persistent_views_added:
             database = await self.db.new(database_category_name,
@@ -506,13 +516,13 @@ if __name__ == "__main__":
     except:
         pass
     bot = MinatoNamikazeBot()
-    cog_dir = BASE_DIR / "cogs"
-    for filename in list(set(os.listdir(cog_dir))):
-        if os.path.isdir(cog_dir / filename):
-            for i in os.listdir(cog_dir / filename):
+    slash_dir = BASE_DIR / "slash"
+    for filename in list(set(os.listdir(slash_dir))):
+        if os.path.isdir(slash_dir / filename):
+            for i in os.listdir(slash_dir / filename):
                 if i.endswith(".py"):
-                    bot.load_extension(f'cogs.{filename.strip(" ")}.{i[:-3]}')
+                    bot.load_extension(f'slash.{filename.strip(" ")}.{i[:-3]}')
         else:
             if filename.endswith(".py"):
-                bot.load_extension(f"cogs.{filename[:-3]}")
+                bot.load_extension(f"slash.{filename[:-3]}")
     bot.run()

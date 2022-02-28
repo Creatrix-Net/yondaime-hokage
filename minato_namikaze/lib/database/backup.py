b3f3e31e5d8c50c08fff32e874ec173d2165be62
Dhruva Shaw <dhruvashaw@gmail.com>
2022-02-28 15:08:24 +0530

    apply command added

--- minato_namikaze/lib/database/backup.py
+++ minato_namikaze/lib/database/backup.py
@@ -48,9 +48,10 @@ class BackupDatabse:
             if isinstance(i, CategoryChannel):
                 category_channel_update_dict = {
                     "nsfw": i.nsfw,
-                    "position": i.position
+                    "position": i.position,
+                    'role_overwrites': {}
                 }
-                for j in i.overwrites.keys():
+                for j in i.overwrites:
                     role_overwrites = {}
                     if isinstance(j, Role):
                         for key, value in i.overwrites.items():
@@ -71,9 +72,10 @@ class BackupDatabse:
                     'permissions_synced': i.permissions_synced,
                     'video_quality_mode': i.video_quality_mode.name,
                     'category': None if i.category is None else i.category.name,
-                    'user_limit': i.user_limit
+                    'user_limit': i.user_limit,
+                    'role_overwrites': {}
                 }
-                for j in i.overwrites.keys():
+                for j in i.overwrites:
                     role_overwrites = {}
                     if isinstance(j, Role):
                         for key, value in i.overwrites.items():
@@ -95,8 +97,9 @@ class BackupDatabse:
                     'category': None if i.category is None else i.category.name,
                     'topic': i.topic,
                     'permissions_synced': i.permissions_synced,
+                    'role_overwrites': {}
                 }
-                for j in i.overwrites.keys():
+                for j in i.overwrites:
                     role_overwrites = {}
                     if isinstance(j, Role):
                         for key, value in i.overwrites.items():
@@ -118,9 +121,10 @@ class BackupDatabse:
                     'video_quality_mode': i.video_quality_mode.name,
                     'topic': i.topic,
                     'category': None if i.category is None else i.category.name,
-                    'user_limit': i.user_limit
+                    'user_limit': i.user_limit,
+                    'role_overwrites': {}
                 }
-                for j in i.overwrites.keys():
+                for j in i.overwrites:
                     role_overwrites = {}
                     if isinstance(j, Role):
                         for key, value in i.overwrites.items():
@@ -187,10 +191,13 @@ class BackupDatabse:
         :return: :class:`str` is returned when there is a error otherwise a :class:`bool` with True value
         :rtype: Optional[Union[str, bool]]
         """        
-        backup: Optional[discord.Attachment] = self.get_backup_data(code)
+        backup: Optional[discord.Attachment] = await self.get_backup_data(code)
+        if backup is None:
+            return 'No backup with that id exists'
         try:
-            data = await backup.read(use_cached=True)
-        except (discord.HTTPException, discord.Forbidden, discord.NotFound):
+            data = await backup.read()
+        except (discord.HTTPException, discord.Forbidden, discord.NotFound) as e:
+            print(e)
             return 'Can\'t apply due to some permission error'
         data = loads(data)
         guild = self.ctx.guild
@@ -203,54 +210,185 @@ class BackupDatabse:
                     reason = self.reason(code, self.ctx.author),
                     colour=roles[j]['colour'],
                     hoist=roles[j]['hoist'],
-                    position=roles[j]['position'],
                     mentionable=roles[j]['mentionable'],
-                    permission=discord.Permissions(roles[j]['hoist'])
+                    permissions=discord.Permissions(roles[j]['permission'])
                 )
+                try:
+                    if not roles[j]['position'] <= 0:
+                        await role_in_server.edit(position=roles[j]['position'], reason = self.reason(code, self.ctx.author))
+                except (discord.Forbidden , discord.HTTPException, discord.InvalidArgument):
+                    pass
+
             except (commands.RoleNotFound, commands.CommandError, commands.BadArgument):
-                await guild.create_role(
+                created_role = await guild.create_role(
                     reason = self.reason(code, self.ctx.author),
                     name=j,
                     colour=roles[j]['colour'],
                     hoist=roles[j]['hoist'],
-                    position=roles[j]['position'],
                     mentionable=roles[j]['mentionable'],
-                    permission=discord.Permissions(roles[j]['hoist'])
+                    permissions=discord.Permissions(roles[j]['permission'])
                 )
+                try:
+                    if not roles[j]['position'] <= 0:
+                        await created_role.edit(position=roles[j]['position'], reason = self.reason(code, self.ctx.author))
+                except (discord.Forbidden , discord.HTTPException, discord.InvalidArgument):
+                    pass
 
         category_data = data['category_channel']
         for i in category_data:
             try:
                 category_channel = await commands.CategoryChannelConverter().convert(self.ctx, i)
-                role_overwrites = {}
-                for j in category_data[i]["category_channel"]:
-                    for k in j:
-                        object_role_or_member = None
-                        try:
-                            object_role_or_member = await commands.RoleConverter().convert(self.ctx, k)
-                        except (commands.RoleNotFound, commands.CommandError, commands.BadArgument):
-                            try:
-                                object_role_or_member = await commands.MemberConverter().convert(self.ctx, k)
-                            except (commands.MemberNotFound, commands.CommandError, commands.BadArgument):
-                                pass
-                        if object_role_or_member is not None:
-                            role_overwrites.update({object_role_or_member: discord.PermissionOverwrite(allow=j[k]['allow'], deny=j[k]['deny'])})
+                
                 await category_channel.edit(
                     position = category_data[i]["position"],
-                    nsfw = category_data[i]["nsfw"],
                     reason  = self.reason(code, self.ctx.author),
-                    overwrites  = role_overwrites
+                    overwrites  = await self.return_role_overwrites(category_data[i]['role_overwrites']), 
+                    nsfw=category_data[i]['nsfw']
                 )
             except (commands.ChannelNotFound, commands.BadArgument, commands.CommandError):
-                await guild.create_category(
+                created_category = await guild.create_category(
                     reason = self.reason(code, self.ctx.author),
-                    nsfw = category_data[i]['nsfw'],
                     position = category_data[i]["position"],
-                    overwrites  = role_overwrites
+                    overwrites  = await self.return_role_overwrites(category_data[i]['role_overwrites']),
+                    name = i
                 )
-        return True
-
+                try:
+                    await created_category.edit(nsfw = category_data[i]['nsfw'], reason = self.reason(code, self.ctx.author))
+                except (discord.Forbidden , discord.HTTPException, discord.InvalidArgument):
+                    pass
+        
+        text_data = data['text_channels']
+        for i in text_data:
+            try:
+                text_channel = await commands.TextChannelConverter().convert(self.ctx, i)  
+                await text_channel.edit(
+                    position = text_data[i]["position"],
+                    nsfw = text_data[i]["nsfw"],
+                    reason  = self.reason(code, self.ctx.author),
+                    overwrites  = await self.return_role_overwrites(text_data[i]['role_overwrites']),
+                    slowmode_delay = text_data[i]["slowmode_delay"],
+                    default_auto_archive_duration = text_data[i]["default_auto_archive_duration"],
+                    topic = text_data[i]["topic"],
+                    permissions_synced = text_data[i]["permissions_synced"],
+                    category = await self.return_category_channel(text_data[i]["category"])
+                )
+            except (commands.ChannelNotFound, commands.BadArgument, commands.CommandError):
+                created_text = await guild.create_text_channel(
+                    reason = self.reason(code, self.ctx.author),
+                    nsfw = text_data[i]['nsfw'],
+                    position = text_data[i]["position"],
+                    overwrites  = await self.return_role_overwrites(text_data[i]['role_overwrites']),
+                    slowmode_delay = text_data[i]["slowmode_delay"],
+                    topic = text_data[i]["topic"],
+                    category = await self.return_category_channel(text_data[i]["category"]),
+                    name= i
+                )
+                try:
+                    await created_text.edit(default_auto_archive_duration = text_data[i]["default_auto_archive_duration"],permissions_synced = text_data[i]["permissions_synced"], reason = self.reason(code, self.ctx.author))
+                except (discord.Forbidden , discord.HTTPException, discord.InvalidArgument):
+                    pass
+        
+        voice_data = data['voice_channel']
+        for i in voice_data:
+            try:
+                voice_channel = await commands.VoiceChannelConverter().convert(self.ctx, i)  
+                await voice_channel.edit(
+                    position = voice_data[i]["position"],
+                    reason  = self.reason(code, self.ctx.author),
+                    overwrites  = await self.return_role_overwrites(voice_data[i]['role_overwrites']),
+                    bitrate = voice_data[i]["bitrate"],
+                    permissions_synced = voice_data[i]["permissions_synced"],
+                    video_quality_mode = discord.VideoQualityMode[voice_data[i]["video_quality_mode"]],
+                    user_limit = voice_data[i]["user_limit"],
+                    category = await self.return_category_channel(voice_data[i]["category"])
+                )
+            except (commands.ChannelNotFound, commands.BadArgument, commands.CommandError):
+                created_voice = await guild.create_voice_channel(
+                    position = voice_data[i]["position"],
+                    reason  = self.reason(code, self.ctx.author),
+                    overwrites  = await self.return_role_overwrites(voice_data[i]['role_overwrites']),
+                    bitrate = voice_data[i]["bitrate"],
+                    video_quality_mode = discord.VideoQualityMode[voice_data[i]["video_quality_mode"]],
+                    user_limit = voice_data[i]["user_limit"],
+                    category = await self.return_category_channel(voice_data[i]["category"]),
+                    name= i
+                )
+                try:
+                    await created_voice.edit(permissions_synced = voice_data[i]["permissions_synced"], reason = self.reason(code, self.ctx.author))
+                except (discord.Forbidden , discord.HTTPException, discord.InvalidArgument):
+                    pass
+        
+        stage_data = data['stage_channel']
+        for i in stage_data:
+            try:
+                voice_channel = await commands.StageChannelConverter().convert(self.ctx, i)  
+                await voice_channel.edit(
+                    position = stage_data[i]["position"],
+                    reason  = self.reason(code, self.ctx.author),
+                    overwrites  = await self.return_role_overwrites(stage_data[i]['role_overwrites']),
+                    bitrate = stage_data[i]["bitrate"],
+                    permissions_synced = stage_data[i]["permissions_synced"],
+                    video_quality_mode = discord.VideoQualityMode[stage_data[i]["video_quality_mode"]],
+                    user_limit = stage_data[i]["user_limit"],
+                    topic = stage_data[i]["topic"],
+                    category = await self.return_category_channel(stage_data[i]["category"])
+                )
+            except (commands.ChannelNotFound, commands.BadArgument, commands.CommandError):
+                created_stage = await guild.create_stage_channel(
+                    position = stage_data[i]["position"],
+                    reason  = self.reason(code, self.ctx.author),
+                    overwrites  = await self.return_role_overwrites(stage_data[i]['role_overwrites']),
+                    topic = stage_data[i]["topic"],
+                    category = await self.return_category_channel(stage_data[i]["category"]),
+                    name= i
+                )
+                try:
+                    await created_stage.edit(
+                        permissions_synced = voice_data[i]["permissions_synced"], 
+                        reason = self.reason(code, self.ctx.author),
+                        video_quality_mode = discord.VideoQualityMode[stage_data[i]["video_quality_mode"]],
+                        bitrate = stage_data[i]["bitrate"],
+                        user_limit = stage_data[i]["user_limit"],
+                    )
+                except (discord.Forbidden , discord.HTTPException, discord.InvalidArgument):
+                    pass
+        
     
+    async def return_category_channel(self, name: str) -> Optional[discord.CategoryChannel]:
+        """It returns the category channel from the category name
+
+        :param name: Category channel name
+        :type name: str
+        :return: See Above
+        :rtype: Optional[discord.CategoryChannel]
+        """        
+        try:
+            return await commands.CategoryChannelConverter().convert(self.ctx, name)
+        except (commands.ChannelNotFound, commands.BadArgument, commands.CommandError):
+            return
+
+    async def return_role_overwrites(self, data: dict) -> dict:
+        """Return the role overwrites dict with proper formatting
+
+        :param data: Dict containing the role overwrites data
+        :type data: dict
+        :return: Role Overwrites with the proper formatting
+        :rtype: dict
+        """        
+        role_overwrites = {}
+        for j in data:
+            for k in j:
+                object_role_or_member = None
+                try:
+                    object_role_or_member = await commands.RoleConverter().convert(self.ctx, k)
+                except (commands.RoleNotFound, commands.CommandError, commands.BadArgument):
+                    try:
+                        object_role_or_member = await commands.MemberConverter().convert(self.ctx, k)
+                    except (commands.MemberNotFound, commands.CommandError, commands.BadArgument):
+                        pass
+                if object_role_or_member is not None:
+                    role_overwrites.update({object_role_or_member: discord.PermissionOverwrite(allow=j[k]['allow'], deny=j[k]['deny'])})
+        return role_overwrites
 
     @staticmethod
     def reason(code:int, author:Optional[Union[discord.Member, discord.User]] = None) -> str:

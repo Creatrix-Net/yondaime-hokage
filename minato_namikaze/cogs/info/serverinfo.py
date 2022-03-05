c906137c9833e0da413c38766e629054115436bc
Dhruva Shaw <dhruvashaw@gmail.com>
2022-03-05 21:32:39 +0530

    removed serverdump

--- minato_namikaze/cogs/info/serverinfo.py
+++ minato_namikaze/cogs/info/serverinfo.py
@@ -15,69 +15,6 @@ class Info(commands.Cog):
     def display_emoji(self) -> discord.PartialEmoji:
         return discord.PartialEmoji(name="\N{INFORMATION SOURCE}")
 
-    @commands.command(
-        name="serverdump",
-        description="Sends info to my developer that you have added me",
-    )
-    @commands.cooldown(1, 1080, commands.BucketType.guild)
-    @commands.has_permissions(administrator=True)
-    @commands.guild_only()
-    async def serverdump(self, ctx):
-        """Dumps server name to the developer"""
-        c = (self.bot.get_channel(813954921782706227) if not self.bot.local
-             else self.bot.get_channel(869238107524968479))
-        if ctx.guild.id in (747480356625711204, 869085099470225508):
-            b = await ctx.send("** Okay updating info ! **")
-            n = 1
-            for guild in ctx.bot.guilds:
-                e = discord.Embed(
-                    title=f"In **{guild.name}**",
-                    description="Was added",
-                    color=discord.Color.green(),
-                )
-                if guild.icon:
-                    e.set_thumbnail(url=guild.icon.url)
-                if guild.banner:
-                    e.set_image(url=guild.banner.with_format("png").url)
-                n += 1
-                e.add_field(name="**Total Members**", value=guild.member_count)
-                e.add_field(
-                    name="**Bots**",
-                    value=sum(1 for member in guild.members if member.bot),
-                )
-                e.add_field(name="**Region**",
-                            value=str(guild.region).capitalize(),
-                            inline=True)
-                e.add_field(name="**Server ID**", value=guild.id, inline=True)
-                await c.send(embed=e)
-            await c.send(f"In total {n} servers")
-            await ctx.send(
-                "**Updated ! Please check the <#813954921782706227>**")
-        else:
-            a = await ctx.send("**Sending the info to my developer**")
-            e = discord.Embed(
-                title=f"In **{ctx.guild.name}**",
-                description=f"Bumped by **{ctx.message.author}**",
-                color=0x2ECC71,
-            )
-            if ctx.guild.icon:
-                e.set_thumbnail(url=ctx.guild.icon.url)
-            if ctx.guild.banner:
-                e.set_image(url=ctx.guild.banner.with_format("png").url)
-            e.add_field(name="**Total Members**", value=ctx.guild.member_count)
-            e.add_field(
-                name="**Bots**",
-                value=sum(1 for member in ctx.guild.members if member.bot),
-            )
-            e.add_field(name="**Region**",
-                        value=str(ctx.guild.region).capitalize(),
-                        inline=True)
-            e.add_field(name="**Server ID**", value=ctx.guild.id, inline=True)
-            await c.send(embed=e)
-            await ctx.send(
-                f'Sent the info to developer that "**I am in __{ctx.guild.name}__ guild**" , {ctx.author.mention} 😉'
-            )
-
     @commands.command()
     @commands.guild_only()
     async def avatar(self, ctx, *, user: discord.Member = None):

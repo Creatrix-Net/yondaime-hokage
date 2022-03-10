f6ba674851dc1ed8f2ad1386644b8828fee45aaf
Dhruva Shaw <dhruvashaw@gmail.com>
2022-03-11 00:12:06 +0530

    updated music error msg and forced musc to use embeds

---
+++ minato_namikaze/lib/classes/music.py
@@ -0,0 +1,10 @@
+from discord.ext import commands
+
+class NoChannelProvided(commands.CommandError):
+    """Error raised when no suitable voice channel was supplied."""
+    pass
+
+
+class IncorrectChannelError(commands.CommandError):
+    """Error raised when commands are issued outside of the players session channel."""
+    pass

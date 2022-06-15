c4c4471208ad3e5cc66ee63a361583c040785547
Miku Chan <34207400+Dhruvacube@users.noreply.github.com>
2022-06-15 23:09:09 +0530

    added logging and hybrid command to vocaloid

--- minato_namikaze/cogs/anime_and_vocaloids/vocaloid.py
+++ minato_namikaze/cogs/anime_and_vocaloids/vocaloid.py
@@ -3,12 +3,13 @@ from typing import TYPE_CHECKING
 import discord
 from discord.ext import commands
 from lib.functions import meek_api
+import logging
 
 if TYPE_CHECKING:
     from lib import Context
-
     from ... import MinatoNamikazeBot
 
+log = logging.getLogger(__name__)
 
 class Vocaloid(commands.Cog):
     def __init__(self, bot: "MinatoNamikazeBot"):
@@ -19,109 +20,109 @@ class Vocaloid(commands.Cog):
     def display_emoji(self) -> discord.PartialEmoji:
         return discord.PartialEmoji(name="miku_wave", id=940993740967931914)
 
-    @commands.command()
+    @commands.hybrid_command()
     @commands.cooldown(1, 2, commands.BucketType.user)
     async def rin(self, ctx: "Context"):
         """Rin kawai picture"""
         await ctx.send(embed=await meek_api("rin"))
 
-    @commands.command()
+    @commands.hybrid_command()
     @commands.cooldown(1, 2, commands.BucketType.user)
     async def una(self, ctx: "Context"):
         """Una kawai picture"""
         await ctx.send(embed=await meek_api("una"))
 
-    @commands.command()
+    @commands.hybrid_command()
     @commands.cooldown(1, 2, commands.BucketType.user)
     async def gumi(self, ctx: "Context"):
         """Gumi kawai picture"""
         await ctx.send(embed=await meek_api("gumi"))
 
-    @commands.command()
+    @commands.hybrid_command()
     @commands.cooldown(1, 2, commands.BucketType.user)
     async def ia(self, ctx: "Context"):
         """Ia kawai picture"""
         await ctx.send(embed=await meek_api("ia"))
 
-    @commands.command()
+    @commands.hybrid_command()
     @commands.cooldown(1, 2, commands.BucketType.user)
     async def luka(self, ctx: "Context"):
         """Luka kawai picture"""
         await ctx.send(embed=await meek_api("luka"))
 
-    @commands.command()
+    @commands.hybrid_command()
     @commands.cooldown(1, 2, commands.BucketType.user)
     async def fukase(self, ctx: "Context"):
         """Fukase kawai picture"""
         await ctx.send(embed=await meek_api("fukase"))
 
-    @commands.command()
+    @commands.hybrid_command()
     @commands.cooldown(1, 2, commands.BucketType.user)
     async def miku(self, ctx: "Context"):
         """Hatsune Miku kawai picture"""
         await ctx.send(embed=await meek_api("miku"))
 
-    @commands.command(name="len")
+    @commands.hybrid_command(name="len")
     @commands.cooldown(1, 2, commands.BucketType.user)
     async def _len(self, ctx: "Context"):
         """Len kawai picture"""
         await ctx.send(embed=await meek_api("len"))
 
-    @commands.command()
+    @commands.hybrid_command()
     @commands.cooldown(1, 2, commands.BucketType.user)
     async def kaito(self, ctx: "Context"):
         """Kaito kawai picture"""
         await ctx.send(embed=await meek_api("kaito"))
 
-    @commands.command()
+    @commands.hybrid_command()
     @commands.cooldown(1, 2, commands.BucketType.user)
     async def teto(self, ctx: "Context"):
         """Teto kawai picture"""
         await ctx.send(embed=await meek_api("teto"))
 
-    @commands.command()
+    @commands.hybrid_command()
     @commands.cooldown(1, 2, commands.BucketType.user)
     async def meiko(self, ctx: "Context"):
         """Meiko kawai picture"""
         await ctx.send(embed=await meek_api("meiko"))
 
-    @commands.command()
+    @commands.hybrid_command()
     @commands.cooldown(1, 2, commands.BucketType.user)
     async def yukari(self, ctx: "Context"):
         """Yukari kawai picture"""
         await ctx.send(embed=await meek_api("yukari"))
 
-    @commands.command()
+    @commands.hybrid_command()
     @commands.cooldown(1, 2, commands.BucketType.user)
     async def miki(self, ctx: "Context"):
         """Miki kawai picture"""
         await ctx.send(embed=await meek_api("miki"))
 
-    @commands.command()
+    @commands.hybrid_command()
     @commands.cooldown(1, 2, commands.BucketType.user)
     async def lily(self, ctx: "Context"):
         """Lily kawai picture"""
         await ctx.send(embed=await meek_api("lily"))
 
-    @commands.command()
+    @commands.hybrid_command()
     @commands.cooldown(1, 2, commands.BucketType.user)
     async def mayu(self, ctx: "Context"):
         """Mayu kawai picture"""
         await ctx.send(embed=await meek_api("mayu"))
 
-    @commands.command()
+    @commands.hybrid_command()
     @commands.cooldown(1, 2, commands.BucketType.user)
     async def aoki(self, ctx: "Context"):
         """Aoki kawai picture"""
         await ctx.send(embed=await meek_api("aoki"))
 
-    @commands.command()
+    @commands.hybrid_command()
     @commands.cooldown(1, 2, commands.BucketType.user)
     async def zola(self, ctx: "Context"):
         """Zola kawai picture"""
         await ctx.send(embed=await meek_api("zola"))
 
-    @commands.command()
+    @commands.hybrid_command()
     @commands.cooldown(1, 2, commands.BucketType.user)
     async def diva(self, ctx: "Context"):
         """Random picturs from Project Diva"""
